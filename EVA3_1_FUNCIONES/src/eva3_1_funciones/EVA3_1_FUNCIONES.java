6338749902d2011ec4c60e457a29a3f654afe546
luis011954 <138296604+luis011954@users.noreply.github.com>
2023-11-23 17:16:03 -0600

    Add files via upload

---
+++ EVA3_1_FUNCIONES/src/eva3_1_funciones/EVA3_1_FUNCIONES.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_1_funciones;
+
+
+/**
+ *
+ * @author clitx
+ */
+public class EVA3_1_FUNCIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //llamanda a funcion o procedimiento
+       imprimirMensaje("Hola mundo!!");
+
+    }
+    
+    public static void imprimirMensaje(String mensaje){
+        System.out.println(mensaje);
+    }
+    
+}
