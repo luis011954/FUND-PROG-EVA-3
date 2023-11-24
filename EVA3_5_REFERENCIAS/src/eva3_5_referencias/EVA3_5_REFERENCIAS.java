21ebe195e7cb3f991e5581a48355355cf574ce30
luis011954 <138296604+luis011954@users.noreply.github.com>
2023-11-24 17:53:58 -0600

    Add files via upload

---
+++ EVA3_5_REFERENCIAS/src/eva3_5_referencias/EVA3_5_REFERENCIAS.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_5_referencias;
+
+/**
+ *
+ * @author clitx
+ */
+public class EVA3_5_REFERENCIAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] arreglo = new int[10];
+        System.out.println(arreglo);
+    }
+    
+}
