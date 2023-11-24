20d4e94016627ea88031831a33b3940f31260c1a
luis011954 <138296604+luis011954@users.noreply.github.com>
2023-11-24 17:53:38 -0600

    Add files via upload

---
+++ EVA3_4_FUNCION_ARREGLO/src/eva3_4_funcion_arreglo/EVA3_4_FUNCION_ARREGLO.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_4_funcion_arreglo;
+
+import java.awt.BorderLayout;
+
+/**
+ *
+ * @author clitx
+ */
+public class EVA3_4_FUNCION_ARREGLO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] arreglo;
+        arreglo = crearArreglo(10);
+        for(int valor: arreglo)
+            System.out.print("["+ valor +"]");
+    }
+    
+    public static int[] crearArreglo (int tama){
+        int[] arreglo = new int[tama];
+        for (int i = 0; i < arreglo.length; i++) {
+            arreglo[i] = (int)(Math.random() * 100);
+            
+        }
+        return arreglo;
+    }
+    
+}
